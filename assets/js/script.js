b733a81bf726410ab4e6f81c2633fab7dddf657f
Suganya <suganya.parthiban2002@gmail.com>
2023-03-21 10:57:30 +0530

    Add. Leap year or not usinf if-else...

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// console.log("Hello")
+
+
+// var a="suganya"
+// console.log(a)
+
+// let monthofbirth="january"
+// console.log("Month of birth is " +monthofbirth)
+
+// const dateofbirth="26"
+// console.log("Date of birth is " +dateofbirth)
+// let dateofbirth="25"
+// console.log("Date of birth is " +dateofbirth")
+
+
+// var a=20
+// var b="20"
+// var c=a%b
+// console.log(c)
+
+// console.log(a===b)
+
+
+let a=2019
+if(a%400==0){
+    console.log("It is a leap year")
+}
+else if(a%100==0){
+    console.log("It is not a leap year")
+}
+else if(a%4==0){
+    console.log("it is a leap year")
+}
+else{
+    console.log("It is not a leap year")
+}
